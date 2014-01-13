c9dfca75751a5dee1f88fc5de932ea63bd015865
Anuj Arora <anuj@Quant.(none)>
2014-01-13 15:09:58 -0500

    modified pi.py

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,10 +1,7 @@
 # Find PI to the Nth Digit
 
-from math import *
+import math
 
-digits = raw_input('Enter number of digits to round PI to: ')
+digits = raw_input("Enter number of digits to show pi: ")
 
-# print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string formatting
-
-# calculate pi using Machin-like Formula
-print ('{0:.%df}' % min(30, int(digits))).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
+print "{0:.{digits}f}".format(math.pi, digits = digits)
